d66cd5002dbac58720d8f0f03b2078cab8a8b5e2
adirannforever <adriangus120o@gmail.com>
2026-08-03 17:41:36 -0600

    fix: optimizacion del codigo de la conexion a la BD

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,12 +7,10 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  max: 10,               
-  idleTimeoutMillis: 30000,  
-  connectionTimeoutMillis: 5000, 
+  ssl: true,
+  connectionTimeoutMillis: 10000,
+  idleTimeoutMillis: 30000,
+  max: 10,
 });
 
 pool.connect((err, client, release) => {
