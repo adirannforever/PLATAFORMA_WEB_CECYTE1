5db1a6f0bf0bcfac839876925cf63d8c101cbdb6
adirannforever <adriangus120o@gmail.com>
2026-08-04 00:02:55 -0600

    fix: eliminacion de redireccionamiento erroneo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -15,9 +15,7 @@ api.interceptors.response.use(
   (error) => {
     if (error.response?.status === 401) {
 
-      if (window.location.pathname !== '/login') {
-        window.location.href = '/login';
-      }
+
     }
     return Promise.reject(error);
   }
